1bbda14470237c43870b23540325f5b19d180ea3
Driccioli <driccioli53@gmail.com>
2021-06-16 15:28:28 +0200

    Aggiunta tabella bonus per Snack 4

--- snack-4/studenti.js
+++ snack-4/studenti.js
@@ -26,3 +26,29 @@ for(var i=0; i<students.length; i++){
     console.log(students[i]);
 }
 
+function generateTableHead(table) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+      }
+  }
+
+  function generateTable(table, data) {
+    for (let element of data) {
+      let row = table.insertRow();
+      for (key in element) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+  }
+
+  var data = Object.keys(students[0]);
+ 
+  generateTable(output, students);
+  generateTableHead(output);
